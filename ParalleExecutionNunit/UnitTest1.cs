5d22bf0a01a2086224370aeba17200996b32c352
Usama Mushtaq <usama.mushtaq@ids-astra.com>
2023-11-30 15:42:26 +0500

    Add project files.

---
+++ ParalleExecutionNunit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ParalleExecutionNunit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
